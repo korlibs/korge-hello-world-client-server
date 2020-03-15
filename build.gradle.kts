c9e38eae56e372e37b03ce17a8a253a73f43c29c
soywiz <soywiz@gmail.com>
2020-03-15 13:19:21 +0100

    More work on the skeleton

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,13 @@
+buildscript {
+    val korgePluginVersion: String by project
 
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+    }
+}
