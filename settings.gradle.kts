be4ed44d39ada24a79ac917cbc0ed0318112740a
soywiz <soywiz@gmail.com>
2023-05-14 07:08:30 +0200

    Update to KorGE 4.0.0 and support using kproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement { repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }  }
 
-//plugins {
-//    //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
-//    id("com.soywiz.kproject.settings") version "0.1.2"
-//}
+plugins {
+    //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
+    id("com.soywiz.kproject.settings") version "0.2.7"
+}
 
-//kproject("./deps")
+kproject("./deps")
 
 include(":shared")
 include(":client")
